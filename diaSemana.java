c353c4c847d86bd9995885572f309f285ab334c5
dgonrom2803 <dgonrom2803@g.educaand.es>
2022-11-02 08:48:19 +0100

    Create diaSemana.java

---
+++ diaSemana.java
@@ -0,0 +1,22 @@
+public class diaSemana {
+    public static void main(String[] args) {
+        int dia = 2;
+        if (dia == 1)
+            System.out.println("Lunes");
+        else if (dia == 2)
+            System.out.println("Martes");
+        else if (dia == 3)
+            System.out.println("Miércoles");
+        else if (dia == 4)
+            System.out.println("Jueves");
+        else if (dia == 5)
+            System.out.println("Viernes");
+        else if (dia == 6 || dia == 7)
+            System.out.println("Finde");
+        
+
+
+        
+
+    }
+}
